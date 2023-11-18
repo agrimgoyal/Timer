7df2f96e8b535ee72a38235d73474ab6ffb1c6e6
agrimgoyal <110822763+agrimgoyal@users.noreply.github.com>
2023-11-18 19:10:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Calculate the target time as 12:00 AM today
-    var now = new Date();
-    var targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    // Set the target time as 12:00 AM on November 19, 2023
+    var targetTime = new Date('November 19, 2023 00:00:00').getTime();
 
     var countdownFunction = setInterval(function() {
         var now = new Date().getTime();
         var distance = targetTime - now;
 
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
-        document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
-        document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
-
         if (distance < 0) {
             clearInterval(countdownFunction);
-            document.getElementById("hours").textContent = "00";
-            document.getElementById("minutes").textContent = "00";
-            document.getElementById("seconds").textContent = "00";
+            // Display only one large knife emoji
+            var countdownElement = document.querySelector('.countdown');
+            countdownElement.innerHTML = '<span style="font-size: 100px;">&#128298;</span>';
+        } else {
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+            document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+            document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
         }
     }, 1000);
 });
